corteza-project/corteza

Add fn to get significant roles for res+op from RBAC rules

pkg/rbac/ruleset_utils.go
@@ -128,3 +128,84 @@ func (set RuleSet) ByRole(roleID uint64) RuleSet {
 	})
 	return out
 }
+
+// Significant roles (sigRoles) returns two list of significant roles.
+//
+// First slice are roles that are allowed to perform an operation on a specific resource (directly or indirectly),
+// 2nd slice are roles that are denied the op.
+func (set RuleSet) sigRoles(res Resource, op Operation) (aRR, dRR []uint64) {
+	if !res.IsAppendable() {
+		// nothing to do here, we need a direct resource id (level=0)
+		return
+	}
+
+	const (
+		dirRes = 0
+		indRes = 1
+	)
+
+	var (
+		rr = map[Access]map[int][]uint64{
+			Allow: {
+				dirRes: make([]uint64, 0),
+				indRes: make([]uint64, 0),
+			},
+			Deny: {
+				dirRes: make([]uint64, 0),
+				indRes: make([]uint64, 0),
+			},
+		}
+	)
+
+	// Extract all relevant rules (by op and resource) and group them by
+	// access and distance (rules for direct resources and rules for indirect resources)
+	for _, r := range set {
+		if r.Operation != op {
+			continue
+		}
+
+		if r.Resource == res {
+			// direct rules
+			rr[r.Access][dirRes] = append(rr[r.Access][dirRes], r.RoleID)
+		}
+
+		if r.Resource.IsAppendable() && r.Resource.TrimID() == res.TrimID() {
+			// rules on all resources of this type
+			rr[r.Access][indRes] = append(rr[r.Access][indRes], r.RoleID)
+		}
+	}
+
+	// Process all extracted roles and make sure that are filtered and ordered by relevance:
+	//  1. list of roles with denied operation directly on the resource
+	dRoles := slice.ToUint64BoolMap(rr[Deny][dirRes])
+
+	//  2. list of roles with allowed operation directly on the resource
+	aRoles := make(map[uint64]bool)
+	for _, r := range rr[Allow][dirRes] {
+		aRoles[r] = !dRoles[r]
+	}
+
+	//  3. list of roles with denied operation indirectly on the resource
+	for _, r := range rr[Deny][indRes] {
+		dRoles[r] = true
+	}
+
+	//  4. list of roles with allowed operation indirectly on the resource
+	for _, r := range rr[Allow][indRes] {
+		aRoles[r] = !dRoles[r]
+	}
+
+	for r, chk := range aRoles {
+		if chk {
+			aRR = append(aRR, r)
+		}
+	}
+
+	for r, chk := range dRoles {
+		if chk {
+			dRR = append(dRR, r)
+		}
+	}
+
+	return
+}

pkg/rbac/service.go
@@ -32,6 +32,7 @@ type (
 		Grant(ctx context.Context, wl Whitelist, rules ...*Rule) (err error)
 		Watch(ctx context.Context)
 		FindRulesByRoleID(roleID uint64) (rr RuleSet)
+		SignificantRoles(res Resource, op Operation) (aRR, dRR []uint64)
 		Rules() (rr RuleSet)
 		Reload(ctx context.Context)
 	}
@@ -206,6 +207,16 @@ func (svc *service) Reload(ctx context.Context) {
 	if err == nil {
 		svc.rules = rr
 	}
+}
+
+// SignificantRoles returns two list of significant roles.
+//
+// See sigRoles on rules for more details
+func (svc *service) SignificantRoles(res Resource, op Operation) (aRR, dRR []uint64) {
+	svc.l.Lock()
+	defer svc.l.Unlock()
+
+	return svc.rules.sigRoles(res, op)
 }
 
 func (svc service) flush(ctx context.Context) (err error) {

zzhkiller/corteza-server

pkg/rbac/ruleset_utils.go
@@ -1,5 +1,9 @@
 package rbac
 
+import (
+	"github.com/cortezaproject/corteza-server/pkg/slice"
+)
+
 func merge(base RuleSet, new ...*Rule) (out RuleSet) {
 	var (
 		o    int
@@ -77,6 +81,87 @@ func flushable(set RuleSet) (deletable, updatable, final RuleSet) {
 		}
 
 		final = append(final, &c)
+	}
+
+	return
+}
+
+// Significant roles (sigRoles) returns two list of significant roles.
+//
+// First slice are roles that are allowed to perform an operation on a specific resource (directly or indirectly),
+// 2nd slice are roles that are denied the op.
+func (set RuleSet) sigRoles(res string, op string) (aRR, dRR []uint64) {
+	if hasWildcards(res) {
+		// nothing to do here, we need a direct resource id (level=0)
+		return
+	}
+
+	const (
+		dirRes = 0
+		indRes = 1
+	)
+
+	var (
+		rr = map[Access]map[int][]uint64{
+			Allow: {
+				dirRes: make([]uint64, 0),
+				indRes: make([]uint64, 0),
+			},
+			Deny: {
+				dirRes: make([]uint64, 0),
+				indRes: make([]uint64, 0),
+			},
+		}
+	)
+
+	// Extract all relevant rules (by op and resource) and group them by
+	// access and distance (rules for direct resources and rules for indirect resources)
+	for _, r := range set {
+		if r.Operation != op {
+			continue
+		}
+
+		if r.Resource == res {
+			// direct rules
+			rr[r.Access][dirRes] = append(rr[r.Access][dirRes], r.RoleID)
+		}
+
+		if matchResource(r.Resource, res) {
+			// rules on all resources of this type
+			rr[r.Access][indRes] = append(rr[r.Access][indRes], r.RoleID)
+		}
+	}
+
+	// Process all extracted roles and make sure that are filtered and ordered by relevance:
+	//  1. list of roles with denied operation directly on the resource
+	dRoles := slice.ToUint64BoolMap(rr[Deny][dirRes])
+
+	//  2. list of roles with allowed operation directly on the resource
+	aRoles := make(map[uint64]bool)
+	for _, r := range rr[Allow][dirRes] {
+		aRoles[r] = !dRoles[r]
+	}
+
+	//  3. list of roles with denied operation indirectly on the resource
+	for _, r := range rr[Deny][indRes] {
+		dRoles[r] = true
+	}
+
+	//  4. list of roles with allowed operation indirectly on the resource
+	for _, r := range rr[Allow][indRes] {
+		aRoles[r] = !dRoles[r]
+	}
+
+	for r, chk := range aRoles {
+		if chk {
+			aRR = append(aRR, r)
+		}
+	}
+
+	for r, chk := range dRoles {
+		if chk {
+			dRR = append(dRR, r)
+		}
 	}
 
 	return

pkg/rbac/service.go
@@ -248,6 +248,16 @@ func (svc *service) flush(ctx context.Context) (err error) {
 	return
 }
 
+// SignificantRoles returns two list of significant roles.
+//
+// See sigRoles on rules for more details
+func (svc *service) SignificantRoles(res Resource, op string) (aRR, dRR []uint64) {
+	svc.l.Lock()
+	defer svc.l.Unlock()
+
+	return svc.rules.sigRoles(res.RbacResource(), op)
+}
+
 func (svc service) String() (out string) {
 	tpl := "%-5v %-20s to %-20s %-30s\n"
 	out += strings.Repeat("-", 120) + "\n"